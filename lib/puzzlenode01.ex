7d15dd9bc109204a3e0e1c501af360f0ef22bb3d
Patrick Carver <patrickcarver@gmail.com>
2018-05-06 23:19:46 -0500

    initial creation of project

---
+++ lib/puzzlenode01.ex
@@ -0,0 +1,18 @@
+defmodule Puzzlenode01 do
+  @moduledoc """
+  Documentation for Puzzlenode01.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Puzzlenode01.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/puzzlenode01_test.exs
@@ -0,0 +1,8 @@
+defmodule Puzzlenode01Test do
+  use ExUnit.Case
+  doctest Puzzlenode01
+
+  test "greets the world" do
+    assert Puzzlenode01.hello() == :world
+  end
+end
