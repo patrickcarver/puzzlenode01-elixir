0893188ecc5276e6fefd72b60ecd7c63fd94ab19
Patrick Carver <patrickcarver@gmail.com>
2018-05-11 22:19:06 -0500

    ensure inverse groups have inverse rates

--- lib/puzzlenode01/rates/inverse_grouper.ex
+++ lib/puzzlenode01/rates/inverse_grouper.ex
@@ -10,11 +10,22 @@ defmodule Puzzlenode01.Rates.InverseGrouper do
   end
 
   def process([rate | tail], inverses) do
-    inverse_rate = Rate.find_inverse(tail, rate)
+    inverse_rate = find_inverse(tail, rate)
 
     updated_inverses = [{rate, inverse_rate} | inverses]
     updated_rates = List.delete(tail, inverse_rate)
 
     process(updated_rates, updated_inverses)
+  end
+
+  def find_inverse(tail, rate) do
+    inverse_rate = Rate.find_inverse(tail, rate)
+
+    case inverse_rate do
+      nil -> 
+        Rate.inverse(rate)
+      _ ->
+        inverse_rate
+    end
   end
 end
